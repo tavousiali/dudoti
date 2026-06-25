33e30b3008f6f9a142d444d74d9b8578059c36cc
tavousi.ali <tavousi.ali@gmail.com>
2026-06-25 10:49:13 +0330

    End of hero section

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -4,14 +4,15 @@ export default function HeroSection() {
   return (
     <section
       className="
-        flex min-h-[calc(100vh-73px)]
+        flex
+        min-h-[calc(100vh-73px)]
         items-center
         justify-center
         bg-[radial-gradient(circle,_rgba(255,215,0,1)_0%,_rgba(255,140,0,1)_50%)]
       "
     >
       <div className="flex flex-col items-center px-5 py-10">
-        <div className="w-full max-w-85">
+        <div className="w-full">
           <Image
             src="/images/Homepage-Hero.png"
             alt="Dudoti Hero"
@@ -22,10 +23,30 @@ export default function HeroSection() {
           />
         </div>
 
-        <div className="mt-3 text-center">
-          <h1 className="text-[32px] font-bold text-white">دودوتی</h1>
+        <div className="mt-2 text-center">
+          <h1 className="relative inline-flex items-center justify-center text-4xl font-bold text-white">
+            <span
+              className="absolute right-full mr-4 text-[30px]"
+              style={{
+                fontFamily: "icomoon",
+                fontWeight: "normal",
+              }}
+            >
+              {"\ue910"}
+            </span>
+            دودوتی
+            <span
+              className="absolute left-full ml-4 text-[30px]"
+              style={{
+                fontFamily: "icomoon",
+                fontWeight: "normal",
+              }}
+            >
+              {"\ue90c"}
+            </span>
+          </h1>
 
-          <p className="mx-auto mt-4 max-w-95 text-lg leading-7 font-bold text-black">
+          <p className="mx-auto mt-5 max-w-96 text-lg leading-7 font-bold text-black">
             تولیدکننده محصولاتی برای زندگی آسوده و سلامت با سگ‌ها، گربه‌ها و
             جوندگان خانگی
           </p>
