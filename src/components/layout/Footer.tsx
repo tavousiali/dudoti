86f04458060a6c8ca40ea566c5ada6551ab0c049
tavousi.ali <tavousi.ali@gmail.com>
2026-07-07 20:54:19 +0330

    Fix footer responsive

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,26 +4,30 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <footer dir="rtl">
-            <div className="relative overflow-hidden bg-[#f92f25] text-white">
+            <div className="relative overflow-visible bg-[#f92f25] text-white">
 
                 {/* Divider */}
-                <div className="absolute mt-16 left-0 right-0 h-px bg-white/70 z-0" />
+                <div className="absolute top-12 left-0 right-0 h-px bg-white/70" />
 
-                <div className="relative z-10 mx-auto flex min-h-80 max-w-7xl items-start justify-end px-5 pt-6 pb-4 sm:px-8 lg:px-12">
+                <div className="relative mx-auto flex flex-col px-5 pt-4 pb-4 sm:px-8 md:max-w-7xl md:flex-row md:justify-end md:px-12">
 
                     {/* Text */}
-                    <div className="flex w-full flex-col items-end pl-32 md:pl-56">
-                        <div className="grid w-full max-w-xl gap-8 text-right md:grid-cols-3">
+                    <div className="w-full md:pl-56">
 
-                            <div>
-                                <Link
-                                    href="#"
-                                    className="text-xs font-bold"
-                                >
-                                    صفحه اصلی
-                                </Link>
-                            </div>
+                        {/* Top menu */}
+                        <div className="flex justify-start">
+                            <Link
+                                href="#"
+                                className="text-xs font-bold"
+                            >
+                                صفحه اصلی
+                            </Link>
+                        </div>
 
+                        {/* Bottom content */}
+                        <div className="mt-8 flex flex-col gap-8 text-right md:grid md:grid-cols-[1fr_1.5fr_1.5fr] md:gap-8">
+
+                            {/* Products */}
                             <div>
                                 <h3 className="mb-3 text-xs font-bold">
                                     محصولات
@@ -48,24 +52,30 @@ export default function Footer() {
                                         </Link>
                                     </li>
                                 </ul>
+                            </div>
 
-                                <h3 className="mt-6 mb-3 text-xs font-bold">
+                            {/* About */}
+                            <div>
+                                <h3 className="mb-3 text-xs font-bold">
                                     درباره دودوتی
                                 </h3>
 
-                                <p className="text-[11px] leading-5">
+                                <p className="text-[11px] leading-5 w-1/2 md:w-60">
                                     غذای تشویقی سگ، غذای تشویقی گربه، غذای تشویقی
                                     جوندگان
                                 </p>
                             </div>
 
+                            {/* Contact */}
                             <div>
                                 <h3 className="mb-3 text-xs font-bold">
                                     تماس با ما
                                 </h3>
 
-                                <p className="text-[11px] leading-5">
-                                    ایمیل: dudoticompany@gmail.com
+                                <p className="text-[11px] leading-5 break-all">
+                                    ایمیل:
+                                    <br />
+                                    dudoticompany@gmail.com
                                 </p>
                             </div>
 
@@ -73,8 +83,10 @@ export default function Footer() {
                     </div>
 
                     {/* Hand */}
-                    <div className="absolute bottom-0 left-0 z-20">
-                        <div className="relative h-52 w-40 md:h-72 md:w-56">
+                    <div className="pointer-events-none flex justify-start absolute bottom-0 left-0 z-20 md:left-0">
+
+                        <div className="relative h-52 w-40 sm:h-48 sm:w-36 md:h-60 md:w-56">
+
                             <Image
                                 src="/images/home/footer-hand.png"
                                 alt=""
@@ -83,8 +95,8 @@ export default function Footer() {
                                 className="object-contain object-bottom"
                             />
 
-                            <div className="absolute top-9 text-start ms-8">
-                                <p className="mb-2 text-[10px] font-bold text-[#f92f25]">
+                            <div className="absolute top-[18%] md:top-[10%] text-start ms-8 md:ms-14">
+                                <p className="mb-1 text-[9px] font-bold text-[#f92f25] md:mb-2 md:text-[12px]">
                                     FOLLOW US
                                 </p>
 
@@ -92,11 +104,15 @@ export default function Footer() {
                                     href="#"
                                     className="text-[#f92f25]"
                                 >
-                                    <span style={{ fontFamily: "icomoon" }} className="text-xl">
+                                    <span
+                                        style={{ fontFamily: "icomoon" }}
+                                        className="text-lg md:text-xl"
+                                    >
                                         {"\ue905"}
                                     </span>
                                 </Link>
                             </div>
+
                         </div>
                     </div>
 
