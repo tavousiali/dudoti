c0a8a8d95fa25f139c352da0134ea8443054bf61
tavousi.ali <tavousi.ali@gmail.com>
2026-07-10 11:50:14 +0330

    Fix HeroSection Responsive

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -6,25 +6,83 @@ export default function HeroSection() {
       className="
         flex
         min-h-[calc(100vh-73px)]
+        md:min-h-[55vh]
         items-center
-        justify-center
-        bg-[radial-gradient(circle,_rgba(255,215,0,1)_0%,_rgba(255,140,0,1)_50%)]
+        overflow-hidden
+        bg-[radial-gradient(circle,_rgba(255,215,0,1)_0%,_rgba(255,140,0,1)_48%,_rgba(255,140,0,1)_100%)]
       "
     >
-      <div className="flex flex-col items-center px-5 py-10">
-        <div className="w-full">
+      <div
+        className="
+          mx-auto
+          flex
+          flex-col
+          md:flex-row
+          w-full
+          max-w-screen-2xl
+          items-center
+          justify-between
+          gap-12
+          px-5
+          py-10
+          md:px-10
+          md:py-0
+          lg:px-16
+          xl:px-24
+        "
+      >
+
+        {/* Hero Image */}
+        <div
+          className="
+            flex
+            flex-1
+            justify-center
+            items-center
+          "
+        >
           <Image
             src="/images/home/hero.png"
-            alt="Dudoti Hero"
-            width={800}
+            alt="Hero"
+            width={900}
             height={900}
             priority
-            className="h-auto w-full"
+            className="
+              h-auto
+              w-120
+              md:w-180
+              p-0
+              md:p-3
+              lg:p-10
+            "
           />
         </div>
 
-        <div className="mt-2 text-center">
-          <h1 className="relative inline-flex items-center justify-center text-4xl font-bold text-white">
+        {/* Text */}
+        <div
+          className="
+            flex-1
+            text-center
+            flex
+            flex-col
+            items-center
+            justify-center
+            gap-4
+          "
+        >
+          <h1
+            className="
+              relative
+              flex
+              items-center
+              font-bold
+              text-white
+              text-6xl
+              md:text-5xl
+              lg:text-6xl
+              xl:text-7xl
+            "
+          >
             <span
               className="absolute right-full mr-4 text-[30px]"
               style={{
@@ -46,11 +104,23 @@ export default function HeroSection() {
             </span>
           </h1>
 
-          <p className="mx-auto mt-5 max-w-96 text-lg leading-7 font-bold text-black">
+          <p
+            className="
+              mt-5
+              text-center
+              font-bold
+              leading-9
+              text-black
+              max-w-lg
+              text-xl
+              xl:text-2xl
+            "
+          >
             تولیدکننده محصولاتی برای زندگی آسوده و سلامت با سگ‌ها، گربه‌ها و
             جوندگان خانگی
           </p>
         </div>
+
       </div>
     </section>
   );
