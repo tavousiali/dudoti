489fd0def18c225b3f87c1b9356b578dfc415ef6
tavousi.ali <tavousi.ali@gmail.com>
2026-07-30 23:13:55 +0330

    Add Product

--- src/components/layout/PageTitle.tsx
+++ src/components/layout/PageTitle.tsx
@@ -2,26 +2,37 @@ type HeadingTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
 type PropType = {
     title: string
+    subtitle?: string
     as?: HeadingTag
 }
 
 export default function PageTitle({
     title,
+    subtitle,
     as: Tag = "h1",
 }: PropType) {
-    return (<Tag className="text-[30px] font-bold text-black">
-        <span
-            className="relative top-2 text-[#ff2f2f]"
-            style={{ fontFamily: "icomoon" }}
-        >
-            {"\ue90c"}
-        </span>
-        <span className="mx-3">{title}</span>
-        <span
-            className="relative top-2 text-[#ff2f2f]"
-            style={{ fontFamily: "icomoon" }}
-        >
-            {"\ue910"}
-        </span>
-    </Tag>)
+    return (
+        <Tag className="text-[30px] font-bold text-black">
+            <span
+                className="relative top-2 text-[#ff2f2f]"
+                style={{ fontFamily: "icomoon" }}
+            >
+                {"\ue90c"}
+            </span>
+            <span className="mx-3">
+                {title}
+                {subtitle && (
+                    <span className="mt-1 block text-base font-normal text-black">
+                        {subtitle}
+                    </span>
+                )}
+            </span>
+            <span
+                className="relative top-2 text-[#ff2f2f]"
+                style={{ fontFamily: "icomoon" }}
+            >
+                {"\ue910"}
+            </span>
+        </Tag>
+    )
 }
